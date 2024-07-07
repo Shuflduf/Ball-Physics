448af4c510f439949134b2d806e8d600fe03f725
Shuflduf <shuflduf@gmail.com>
2024-07-07 15:52:05 -0600

    reactions with other balls

--- Main.gd
+++ Main.gd
@@ -17,18 +17,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	for ball: Ball in balls.get_children():
+	var all_balls = balls.get_children()
+	
+	for ball: Ball in all_balls:
 		ball.force += ball.mass * Vector3(0, GRAVITY, 0)
 		ball.velocity += ball.force / ball.mass * delta
 		
 		if ball.position.y - ball.radius + (ball.velocity.y * delta) < plane_depth:
 			ball.velocity.y *= -damping
 			
-		for other_ball: Ball in balls.get_children():
+		for other_ball: Ball in all_balls:
 			if other_ball == ball:
 				continue
-			if (ball.position - other_ball.position).length() < 1:
-				print("ah")
+			if (ball.position - other_ball.position).length() < ball.radius + other_ball.radius:
+				report_collision(ball, other_ball)
 		
 		ball.position += ball.velocity * delta
 		
@@ -40,10 +42,18 @@ func create_ball(pos):
 	balls.add_child(new_ball)
 
 
-func _on_area_3d_input_event(_c, event: InputEvent, input_position: Vector3, _n, _s) -> void:
+func _on_area_3d_input_event(_c, event, input_position, _n, _s) -> void:
 	if event.is_action_pressed("left_click"):
-		create_ball(input_position + Vector3(0, -plane_depth, 0))
+		create_ball(input_position + Vector3(0, 5, 0))
 
 
-func report_collision(first_ball, second_ball):
-	pass
+func report_collision(first_ball: Ball, second_ball: Ball):
+	var impulse = (first_ball.position + first_ball.velocity)\
+		 - (second_ball.position + second_ball.velocity)
+	
+	impulse = impulse.normalized()
+	
+	print(impulse)
+	
+	first_ball.velocity += impulse
+	second_ball.velocity -= impulse

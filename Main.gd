5f2524de1c1867ca63c46cd07b75bd7323fef47d
Shuflduf <shuflduf@gmail.com>
2024-07-06 19:44:10 -0600

    project setup

---
+++ Main.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var balls: Node3D = $Balls
+
+var ball_velocities: Array[Vector3]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
